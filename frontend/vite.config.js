2ef066b26320e4fe023a1d8be56c5fe0ce1e2b05
VERSER <user@users-MacBook-Pro.local>
2026-04-23 08:23:18 +0200

    Deployment commits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5173',
+        target: 'https://subletmatch.com',
         changeOrigin: true,
         secure: false,
       },
